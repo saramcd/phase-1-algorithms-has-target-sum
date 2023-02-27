13400af3fda191c037e09461774efa89fdb8c967
Sara McDonnell <saramcdonnell32@gmail.com>
2023-02-27 16:53:00 -0500

    lab

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      sum = array[i] + array[j];
+      if (sum === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n2)
 */
 
 /* 
   Add your pseudocode here
+  Iterate through the array, starting with the first index
+    Add the number at the next index, if it matches the target, return true, otherwise, continue
 */
 
 /*
